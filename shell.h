81295a14f36f586bcde0e7dd019e294d3b02e1df
Samuel <ogboyesam@gmail.com>
2023-05-26 22:19:06 -0700

    h

--- shell.h
+++ shell.h
@@ -117,6 +117,7 @@ void fork_cmd(info_t *);
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info_t *, char *, char *);
+ssize_t _nodeIndex(list_t *head, list_t *node);
 
 
 int loophsh(char **);
